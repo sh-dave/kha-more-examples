512d3ece9e2983d9598d4fbaf669f0adb9bf46c0
David Klein <dave@smokinheads.de>
2016-02-09 13:56:04 +0100

    added images

--- stencil-triangles/configure.cmd
+++
@@ -1,7 +0,0 @@
-node ../Kha/make --target flash --to build/flash
-node ../Kha/make --target html5 --to build/html5
-node ../Kha/make --target windows --to build/vs2012-gl --visualstudio vs2012 --graphics opengl
-node ../Kha/make --target windows --to build/vs2012-d3d9 --visualstudio vs2012 --graphics direct3d9
-node ../Kha/make --target windows --to build/vs2012-d3d11 --visualstudio vs2012 --graphics direct3d11
-node ../Kha/make --target windows --to build/vs2012-d3d12 --visualstudio vs2012 --graphics direct3d12
-node ../Kha/make --target android --to build/android
--- stencil-triangles/khafile.js
+++
@@ -1,8 +0,0 @@
-var project = new Project('stencil_triangles');
-project.addSources('src');
-project.addAssets('assets/**');
-
-project.windowOptions.width = 512;
-project.windowOptions.height = 512;
-
-return project;
