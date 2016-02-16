f2d8389251d52ded1b3551dcaf62b0e8b1b9075f
David Klein <dave@smokinheads.de>
2016-02-16 17:25:44 +0100

    added gitignore

--- system-options-playground/configure.cmd
+++ system-options-playground/configure.cmd
@@ -1,4 +1,9 @@
 node Kha/make --target flash --to build/flash
+node Kha/make --target html5 --to build/html
 node Kha/make --target linux --to build/linux
 node Kha/make --target windows --visualstudio vs2012 --to build/vs2012-gl --graphics opengl
-::node Kha/make --target android-native --to build/android-native
+node Kha/make --target windows --visualstudio vs2012 --to build/vs2012-d3d9 --graphics direct3d9
+node Kha/make --target windows --visualstudio vs2012 --to build/vs2012-d3d11 --graphics direct3d11
+node Kha/make --target windows --visualstudio vs2012 --to build/vs2012-d3d12 --graphics direct3d12
+node Kha/make --target android-native --to build/android-native
+node Kha/make --target android --to build/android
